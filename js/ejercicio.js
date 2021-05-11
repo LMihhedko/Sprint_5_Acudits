374bcccf87b9a3b21a87cf6f3ecc44503467a949
LAPTOP-JA5654I3\Liza <lizamihhedko@gmail.com>
2021-05-11 19:23:32 +0200

    first commit

---
+++ js/ejercicio.js
@@ -0,0 +1,29 @@
+function showJokes() {
+	
+	//definir función para llamar a la API de obtención de datos
+
+		const url = 'https://icanhazdadjoke.com';
+        //informar los parámetros de la petición
+			let parametros = {
+				method: 'get', //metodo de envío
+				headers: {'Accept': 'application/json'}
+			}
+
+        fetch(url, parametros)
+        .then(function(respuesta) {
+            if (respuesta.ok) {
+                return respuesta.json();
+            } else {
+                console.log('error en la peticion)');
+            }
+        })
+        .then(function(mensaje) {
+            //console.log(mensaje.joke);
+            let joke = mensaje.joke 
+			document.getElementById('joke').innerHTML = joke
+        })
+        .catch(function(error) {
+            console.log(error);
+        })       
+
+ }
