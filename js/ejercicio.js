31c0467573ea7b13b5e6c0bee2428d9fd6368d87
LAPTOP-JA5654I3\Liza <lizamihhedko@gmail.com>
2021-05-11 19:28:49 +0200

    Ejercicio 3 N2

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,6 +1,8 @@
+window.onload = function() {
+    showWeather();
+  };
+
 function showJokes() {
-	
-	//definir función para llamar a la API de obtención de datos
 
 		const url = 'https://icanhazdadjoke.com';
         //informar los parámetros de la petición
@@ -18,8 +20,7 @@ function showJokes() {
             }
         })
         .then(function(mensaje) {
-            //Ejercicio 1 -> console.log(mensaje.joke);
-            //Ejercicio 2
+            //console.log(mensaje.joke);
             let joke = mensaje.joke 
 			document.getElementById('joke').innerHTML = joke
         })
@@ -28,3 +29,43 @@ function showJokes() {
         })       
 
  }
+
+ 
+
+ function showWeather() {
+     //saber las coordenadas de la persona que entra
+     navigator.geolocation.getCurrentPosition(getLocation);
+
+     function getLocation(pos) {
+        var crd = pos.coords
+        console.log(crd.latitude)
+    
+        const api_key = 'd1cfddacc887d8e7ac4980c1dbd0371f';
+    
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${api_key}`;
+        
+        fetch(url)
+        .then(function(respuesta) {
+            if (respuesta.ok) {
+                return respuesta.json();
+            } else {
+                console.log('error en la peticion)');
+            }
+        })
+        .then(function(mensaje) {
+            let main = mensaje.weather[0].main
+            let description = mensaje.weather[0].description
+            console.log(mensaje)
+            document.getElementById('weather').innerHTML = `Today: ${main} - ${description}`
+        })
+        .catch(function(error) {
+            console.log(error);
+        })  
+    
+    }  
+    
+ }
+
+
+
+
